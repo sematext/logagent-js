5803ab66769ea9b16e25214c1ebe9989930164c8
megastef <stefan.thies@sematext.com>
2016-05-17 12:19:42 +0200

    add disk buffer dir in docker file

--- docker-run-logagent.sh
+++ docker-run-logagent.sh
@@ -5,5 +5,7 @@ export RTAIL_OPTIONS="-s --rtail-host $HOSTNAME --rtail-web-port 80 --rtail-port
 export LOGAGENT_OPTIONS=${LOGAGENT_OPTIONS:-"-s"}
 # default pattern file
 export PATTERN_FILE=${PATTERN_FILE:-"/src/patterns.yml"}
+export LOGSENE_TMP_DIR=/logagent-disk-buffer
+mkdir -p $LOGSENE_TMP_DIR
 echo "logagent -u 514 -t ${LOGSENE_TOKEN} -p $PATTERN_FILE ${LOGAGENT_OPTIONS}"
 logagent -u 514 -t ${LOGSENE_TOKEN} -p $PATTERN_FILE ${LOGAGENT_OPTIONS}

6749c3df83d5e68862d84c781c0b8e7272267d4e
megastef <stefan.thies@sematext.com>
2016-05-31 12:40:04 +0200

    add simple tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 
 describe('Logagent parse JSON', function() {
   it('should return corect message with timestamp', function(done) {
+    this.timeout(150000)
     var Logagent = require('../lib/index.js')
     var la = new Logagent()
     la.parseLine (JSON.stringify({
@@ -22,6 +23,7 @@ describe('Logagent parse JSON', function() {
 
 describe('Logagent parse web server Log', function() {
   it('should return client_ip and status_code', function(done) {
+    this.timeout(150000)
     var Logagent = require('../lib/index.js')
     var la = new Logagent()
     la.parseLine ('190.160.248.117 - - [03/Apr/2016:06:25:38 +0000] "GET /about/ HTTP/1.1" 200 14243 "https://sematext.com/consulting/elasticsearch/" "Mozilla/5.0 (iPhone; CPU iPhone OS 8_1_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12B436 Twitter for iPhone"', 
@@ -38,3 +40,4 @@ describe('Logagent parse web server Log', function() {
     })
   })
 })
+

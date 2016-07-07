2b125e86190b9d407a30b68206fa2ca442b6d229
megastef <stefan.thies@sematext.com>
2016-07-07 23:56:56 +0200

    remove dependency to spm-agent-nodejs

--- bin/logagent.js
+++ bin/logagent.js
@@ -2,18 +2,23 @@
 ':' // ; export MAX_MEM="--max-old-space-size=500"; exec "$(command -v node || command -v nodejs)" --harmony "${NODE_OPTIONS:-$MAX_MEM}" "$0" "$@" 
 'use strict'
 /*
- * @copyright Copyright (c) Sematext Group, Inc. - All Rights Reserved
+ * Licensed to Sematext under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Sematext licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @licence logagent-js is free-to-use, proprietary software.
- * THIS IS PROPRIETARY SOURCE CODE OF Sematext Group, Inc. (Sematext)
- * This source code may not be copied, reverse engineered, or altered for any purpose.
- * This source code is to be used exclusively by users and customers of Sematext.
- * Please see the full license (found in LICENSE in this distribution) for details on its license and the licenses of its dependencies.
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-if (process.env.SPM_TOKEN && process.env.NODE_ENV === 'production') {
-  // in production we monitor this node.js process
-  var spmAgentNodeJs = require('spm-agent-nodejs')
-}
 var consoleLogger = require('../lib/logger.js')
 var fs = require('fs')
 var argv = require('../lib/cli/cliArgs.js')

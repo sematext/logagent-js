e6ee6bb84f07b6e2288e4116e60f7fc4e471deb7
megastef <stefan.thies@sematext.com>
2016-10-23 22:51:41 +0200

    updates docker start script

--- docker-run-logagent.sh
+++ docker-run-logagent.sh
@@ -5,7 +5,8 @@ export RTAIL_OPTIONS="-s --rtail-host $HOSTNAME --rtail-web-port 80 --rtail-port
 export LOGAGENT_OPTIONS=${LOGAGENT_OPTIONS:-"-s"}
 # default pattern file
 export PATTERN_FILE=${PATTERN_FILE:-"/src/patterns.yml"}
-export LOGSENE_TMP_DIR=/logagent-disk-buffer
+export LOGSENE_TMP_DIR=${LOGSENE_TMP_DIR:-/logagent-disk-buffer}
 mkdir -p $LOGSENE_TMP_DIR
-echo "logagent -u 514 --index ${LOGSENE_TOKEN} -p $PATTERN_FILE ${LOGAGENT_OPTIONS}"
-logagent -u 514 --index ${LOGSENE_TOKEN} -p $PATTERN_FILE ${LOGAGENT_OPTIONS}
+export CMD="logagent -u 514 --index ${LOGSENE_TOKEN} -f $PATTERN_FILE ${LOGAGENT_OPTIONS}"
+echo $CMD
+$CMD

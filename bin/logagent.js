a44df4605f8d1fdc5d2257d34bb400e7a356d5dc
megastef <s.thies@bigdata-analyst.de>
2017-09-19 12:01:05 +0200

    add output-files plugin

--- bin/logagent.js
+++ bin/logagent.js
@@ -45,7 +45,8 @@ var moduleAlias = {
   // output plugins
   elasticsearch: '../lib/plugins/output/elasticsearch.js',
   'slack-webhook': '../lib/plugins/output/slack-webhook.js',
-  'output-kafka': '../lib/plugins/output/kafka.js'
+  'output-kafka': '../lib/plugins/output/kafka.js',
+  'output-files': '../lib/plugins/output/files.js'
 }
 
 function LaCli (options) {

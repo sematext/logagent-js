88bc066e2b87fa69e12e438a90da4958b6518edb
megastef <s.thies@bigdata-analyst.de>
2017-10-16 13:45:06 +0200

    implement dynamic response for N documents

--- elasticsearch-http-test/test.sh
+++ elasticsearch-http-test/test.sh
@@ -1,4 +1,5 @@
 cd elasticsearch-test
+
 docker-compose version 
 docker version
 if [[ "$(docker ps)" =~ "elastic" ]]; then 
@@ -6,8 +7,14 @@ if [[ "$(docker ps)" =~ "elastic" ]]; then
   docker-compose rm -f && true
 fi 
 docker-compose up -d 
-sleep 25
+ps -ef |grep elastic-http | awk '{print $2}' | xargs kill 
+../bin/logagent.js --config ../config/examples/elastic-http-input.yml & 
+sleep 20
 
-#curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9900/_bulk --data-binary "@requests
-#sleep 10
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9900/_bulk --data-binary "@requests"
+sleep 30
 curl 'localhost:9200/_cat/indices?v' 2>&1 | grep mytest
+curl 'localhost:9200/mytest/_search' | ../bin/logagent.js -y 
+curl 'localhost:9200/mytest/_count'  | ../bin/logagent.js -y  
+sleep 5
+ps -ef |grep elastic-http | awk '{print $2}' | xargs kill -3

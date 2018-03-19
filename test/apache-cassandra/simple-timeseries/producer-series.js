3ffdee5c8ee8a0d2f7d7ad2ac1578139f3eb8844
Filippo Balicchia <filippo.balicchia@openmindonline.it>
2018-03-19 07:43:21 +0100

    add simple batch

--- test/apache-cassandra/simple-timeseries/producer-series.js
+++ test/apache-cassandra/simple-timeseries/producer-series.js
@@ -1,25 +1,31 @@
 'use strict'
 const cassandra = require('cassandra-driver')
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1']})
-const INSERT_QUERY = 'INSERT INTO logagent.orders_by_day(order_number,date,event_time,amount) VALUE (?,?,?,?)'
+const INSERT_QUERY = 'INSERT INTO logagent.orders_by_day(order_number,date,event_time,amount) VALUES (?,?,?,?)'
 
-    
-    
-//     INSERT INTO orders_by_day(order_number,date,event_time,amount)
-//     VALUES (’1234ABCD’,’2018-04-03′,’2013-04-03 07:01:00′,’2000€’);
-    
-//     INSERT INTO orders_by_day(order_number,date,event_time,amount)
-//     VALUES (’1234ABCD’,’2018-04-03′,’2013-04-03 07:02:00′,’6500€’);
-    
-//     INSERT INTO orders_by_day(order_number,date,event_time,amount)
-//     VALUES (’1234ABCD’,’2018-04-04′,’2013-04-04 07:01:00′,’4000€’);
-    
-//     INSERT INTO orders_by_day(order_number,date,event_time,amount)
-//     VALUES (’1234ABCD’,’2018-04-04′,’2013-04-04 07:02:00′,’6500€’);
+const queries = [
+  {
+    query: 'INSERT INTO logagent.orders_by_day(order_number,date,event_time,amount) VALUES (?,?,?,?)',
+    params: ['1234ABCD','2018-03-02','2018-03-02 07:01:00','2000€']
+  },
+  {
+    query: 'INSERT INTO logagent.orders_by_day(order_number,date,event_time,amount) VALUES (?,?,?,?)',
+    params: ['45678ABCD','2018-03-02','2018-03-02 09:01:00','6500€']
+  },
+  {
+    query: 'INSERT INTO logagent.orders_by_day(order_number,date,event_time,amount) VALUES (?,?,?,?)',
+    params: ['6666ABCD','2018-03-03','2018-03-03 07:01:00','3445€']
+  },
+  {
+    query: 'INSERT INTO logagent.orders_by_day(order_number,date,event_time,amount) VALUES (?,?,?,?)',
+    params: ['9999ABCD','2018-03-03','2018-03-03 09:01:00','2345€']
+  },
+]
+
 
 client.connect()
   .then(function () {
-    const query = "CREATE KEYSPACE IF NOT EXISTS logagent WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1' }"
+    const query = "CREATE KEYSPACE IF NOT EXISTS logagent WITH replication ={'class': 'SimpleStrategy', 'replication_factor': '3' }"
     return client.execute(query)
   })
   .then(function () {
@@ -27,18 +33,20 @@ client.connect()
     return client.execute(query)
   })
   .then(function () {
-    console.log('try to insert data')
-    return client.execute(INSERT_QUERY,['201803030AEDFG','2018-04-03','2013-04-03 07:01:00','2000€'],{ traceQuery: true})
+    console.log('in')
+    //const params = ['2018044000AEEFG','2018-05-03','2013-04-03 07:01:00','2000€']
+    return client.execute(queries,{ prepare: true})
+    //return client.execute(INSERT_QUERY,params,{ prepare: true})
+  })
+  .then(function() {
+    const params = ['2018044000AEEFG', '2018-05-03']
+    const QUERY = 'SELECT * FROM logagent.orders_by_day WHERE order_number= ? AND date= ?'
+    return client.execute(QUERY,params,{ prepare: true})
   })
   .then(function (result) {
-    const order_number = result.info.order_number
-    return client.metadata.getTrace(order_number)
-  })
-  .then(function (trace) {
-    console.log('Trace for the execution of the query:')
-    console.log(trace)
-    console.log('The trace was retrieved successfully')
+    console.log('result ' + JSON.stringify(result))
     client.shutdown()
+
   })
   .catch(function (err) {
     console.error('There was an error', err)

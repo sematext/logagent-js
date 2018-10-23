bd2754ae6dd1ee8bf8fef5121eb7364aaa4187ac
Jens Bissinger <mail@jens-bissinger.de>
2018-10-23 16:55:17 +0200

    Add output prometheus-alertmanager

---
+++ test/output-prometheus-alertmanager-test.js
@@ -0,0 +1,64 @@
+var assert = require('assert')
+var OutputPrometheusAlertmanager = require('../lib/plugins/output/prometheus-alertmanager.js')
+describe('OutputPrometheusAlertmanager', function () {
+  describe('#new', function () {
+    it('throws if no config given', function () {
+      assert.throws(function() {
+        new OutputPrometheusAlertmanager({})
+      }, {
+        name: 'TypeError',
+        message: 'Please specify Prometheus Alertmanager "url"'
+      })
+    })
+
+    it('throws if no config.url given', function () {
+      assert.throws(function() {
+        new OutputPrometheusAlertmanager()
+      }, {
+        name: 'TypeError',
+        message: 'Please specify Prometheus Alertmanager "url"'
+      })
+    })
+
+    it('returns instance', function () {
+      new OutputPrometheusAlertmanager({url: 'foo'})
+    })
+  })
+
+  describe('#buildAlert', function () {
+    it('return alert', function () {
+      var output = new OutputPrometheusAlertmanager({url: 'foo'})
+      assert.deepEqual(output.buildAlert(), {
+        labels: {},
+        annotations: {}
+      })
+    })
+
+    it('return alert with templated values', function () {
+      var output = new OutputPrometheusAlertmanager({
+        url: 'foo',
+        alertTemplate: {
+          generatorURL: 'http://foo?myVarA={myVarA}&myVarB={myVarB}',
+          labels: {
+            myLabel: '{myVarA}-{myVarB}'
+          },
+          annotations: {
+            myAnnotation: '{myVarA} and {myVarB}'
+          }
+        }
+      })
+      assert.deepEqual(output.buildAlert({
+        myVarA: 'valOfMyVarA',
+        myVarB: 'valOfMyVarB'
+      }), {
+        generatorURL: 'http://foo?myVarA=valOfMyVarA&myVarB=valOfMyVarB',
+        labels: {
+          myLabel: 'valOfMyVarA-valOfMyVarB'
+        },
+        annotations: {
+          myAnnotation: 'valOfMyVarA and valOfMyVarB'
+        }
+      })
+    })
+  })
+})
